125ed2d34e681a43a0e74bf249c8e8e8f5535bf3
liguangc <liguangc@chanjet.com>
2015-09-21 17:31:07 +0800

    布局基本的html结构，并添加左右滑动事件

---
+++ js/jquery.slider.js
@@ -0,0 +1,29 @@
+$(function(){
+	//绑定dom事件
+	var i = -1;
+	//使用锁控制动画在规定时间内只能执行一次
+	var lock = false; 
+	$(".prev-btn").on("click", function(){
+		if(!lock){
+			i--;
+			lock = true;
+		}
+		$(".list").animate({
+			left : i * 200
+		}, 500, function(){
+			lock = false;
+		})
+	})
+
+	$(".next-btn").on("click", function(){
+		if(!lock){
+			i++;
+			lock = true;
+		}
+		$(".list").animate({
+			left : i * 200
+		}, 500, function(){
+			lock = false;
+		})
+	})
+})
